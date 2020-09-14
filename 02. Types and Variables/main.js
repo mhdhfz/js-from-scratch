8ff139a108018bd9295ed9ec96de53e269b0118b
Hafiz Mustafa <mhd.hfzz@gmail.com>
2020-09-14 11:14:59 +0800

    types and variables

---
+++ 02. Types and Variables/main.js
@@ -0,0 +1,44 @@
+// Types : Strings
+"this is my string";
+"my second string";
+"my name is hafiz";
+
+// Type : Number
+10;
+500;
+3.14;
+
+// Type: Booleans (truthy / falsy)
+true;
+false;
+
+// Type: specials
+undefined; // variable been created but no assign
+null; // no values
+NaN; // not a number
+
+// Objects
+const user = {
+  // key and value
+  name: "hafiz",
+  username: "mhdhfz",
+};
+
+// array - store multiple item - list of things
+const users = ["hafiz", "ali", " ahmad"];
+const luckyNumbers = [1, 43, 54, 132];
+const whatever = ["mike", 2, true, null, undefined];
+
+const superUsers = [{ name: "jai" }, { name: "mat" }, { name: "loi" }];
+
+// declare variables
+// before es5
+var thing = "first";
+// after es5
+const otherThing = "second";
+const newThing = "third";
+
+// declare but not assign
+let query;
+
+query = "SELECT * FROM db";
